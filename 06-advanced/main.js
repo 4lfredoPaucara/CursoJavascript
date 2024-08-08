852a9f71310369814121d178f6a922bd9f382946
Alfredo Paucara <4lfredo.paucara@gmail.com>
2024-08-07 20:46:57 -0400

    se concluyo con la seccion 11, ya nos falta poco

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
 import { forAwaitComponent } from './src/concepts/08-for-await';
 import { generatorFunctionsComponent } from './src/concepts/09-generatorFunctionsComponent';
+import { generatorsAsyncComponent } from './src/concepts/10-generators-async';
 
 
 
@@ -38,7 +39,7 @@ const element = document.querySelector('.card');
 //asyncAwaitComponent( element );
 //asyncAwait2Component( element );
 //forAwaitComponent ( element );
-
-generatorFunctionsComponent ( element );
+//generatorFunctionsComponent ( element );
+generatorsAsyncComponent ( element );
 
 
